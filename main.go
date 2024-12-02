ef8883308448b67a9a50b6b75bafdfa4d12b5427
0Xero7 <smpsmp13@gmail.com>
2024-12-02 11:39:17 +0530

    fast and amazing

--- main.go
+++ main.go
@@ -10,27 +10,33 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
-	"github.com/ericlagergren/decimal"
 )
 
-const width = 512
-const height = 512
+const width = 1024
+const height = 1024
 
 const centerX float64 = -0.6596510985176695 //.75 // -1.04180483110546
 const centerY float64 = -0.3362177249890653 //0.1               // 0.346342664848392
-const scale = 128
+const scale = 20480
 
 const scaleFactorX = float64(3) / (width * scale)
-const scaleFactorY = float64(3) / (height * scale)
+const scaleFactorY = (float64(3*height) / float64(width)) / (height * scale)
 
 var iterations = 1
 
+const subIterations = 100 * 2
+
 // var value [width][height]AComplex
 
-var zr [width][height]decimal.Big
-var zi [width][height]decimal.Big
-var zr2 [width][height]decimal.Big
-var zi2 [width][height]decimal.Big
+// var zr [width][height]decimal.Big
+// var zi [width][height]decimal.Big
+// var zr2 [width][height]decimal.Big
+// var zi2 [width][height]decimal.Big
+
+var fzr [width][height]float64
+var fzi [width][height]float64
+var fzr2 [width][height]float64
+var fzi2 [width][height]float64
 
 var explodesAt [width][height]int
 var maxExplodesAt = 1
@@ -61,12 +67,12 @@ func DoProcess(pair Pair, completed chan Message) {
 	x := centerX + dx*scaleFactorX
 	y := centerY + dy*scaleFactorY
 
-	for i := range 100 {
-		z1r := zr2[pair.x][pair.y]
-		z1i := zi2[pair.x][pair.y]
+	for i := range subIterations {
+		z1r := fzr2[pair.x][pair.y]
+		z1i := fzi2[pair.x][pair.y]
 
-		if CompareDecimal(AddDecimal(z1r, z1i), Dec(4)) == 1 {
-			explodesAt[pair.x][pair.y] = 100*(iterations-1) + i
+		if z1i+z1r > 4 { // CompareDecimal(AddDecimal(z1r, z1i), Dec(4)) == 1 {
+			explodesAt[pair.x][pair.y] = subIterations*(iterations-1) + i
 			if explodesAt[pair.x][pair.y] > maxExplodesAt {
 				maxExplodesAt = explodesAt[pair.x][pair.y]
 			}
@@ -74,14 +80,23 @@ func DoProcess(pair Pair, completed chan Message) {
 		}
 
 		// Calculate next iteration
-		z3i := AddDecimal(MulDecimal(MulDecimal(Dec(2), zr[pair.x][pair.y]), zi[pair.x][pair.y]), Dec(y)) // 2.0*zr*zi + ci
-		z3r := AddDecimal(SubDecimal(zr2[pair.x][pair.y], zi2[pair.x][pair.y]), Dec(x))                   // zr2 - zi2 + cr
+		// z3i := AddDecimal(MulDecimal(MulDecimal(Dec(2), zr[pair.x][pair.y]), zi[pair.x][pair.y]), Dec(y)) // 2.0*zr*zi + ci
+		// z3r := AddDecimal(SubDecimal(zr2[pair.x][pair.y], zi2[pair.x][pair.y]), Dec(x)) // zr2 - zi2 + cr
 
-		zr[pair.x][pair.y] = z3r
-		zi[pair.x][pair.y] = z3i
+		z3i := float64(2)*fzr[pair.x][pair.y]*fzi[pair.x][pair.y] + y
+		z3r := fzr2[pair.x][pair.y] - fzi2[pair.x][pair.y] + x
 
-		zr2[pair.x][pair.y] = MulDecimal(z3r, z3r)
-		zi2[pair.x][pair.y] = MulDecimal(z3i, z3i)
+		// zr[pair.x][pair.y] = z3r
+		// zi[pair.x][pair.y] = z3i
+
+		fzr[pair.x][pair.y] = z3r
+		fzi[pair.x][pair.y] = z3i
+
+		// zr2[pair.x][pair.y] = MulDecimal(z3r, z3r)
+		// zi2[pair.x][pair.y] = MulDecimal(z3i, z3i)
+
+		fzr2[pair.x][pair.y] = z3r * z3r
+		fzi2[pair.x][pair.y] = z3i * z3i
 	}
 
 	completed <- Message{
@@ -104,32 +119,13 @@ func main() {
 	image := image.NewRGBA(image.Rect(0, 0, width, height))
 	ticker := time.NewTicker(time.Millisecond * 1020)
 
-	// q := make([]Pair, width*height)
-	// for x := range width {
-	// 	for y := range height {
-	// 		q = append(q, Pair{x: x, y: y})
-
-	// 		// dx := float64(x - (width / 2))
-	// 		// dy := float64(y - (height / 2))
-
-	// 		// x1 := centerX + dx*scaleFactorX
-	// 		// y1 := centerY + dy*scaleFactorY
-
-	// 		// init := New(x1, y1)
-	// 		// value[x][y] = *init // - complex(-0.10714993602959, -0.91210639328364)
-	// 	}
-	// }
-
 	go func() {
 		for {
 			exited := false
 			select {
 			case <-ticker.C:
-				// imageLock.Lock()
-				// bImg := GaussianBlur(image, 2)
 				img := canvas.NewImageFromImage(image)
 				w.SetContent(img)
-				// imageLock.Unlock()
 
 			case <-quitCh:
 				exited = true
@@ -149,7 +145,6 @@ func main() {
 				return
 
 			case m := <-workerCompleted:
-				// imageLock.Lock()
 				if explodesAt[m.x][m.y] == 0 {
 					image.Set(m.x, m.y, color.Black)
 				} else {
@@ -157,7 +152,6 @@ func main() {
 					col := spectral_color(float64(300) + div*float64(explodesAt[m.x][m.y]))
 					image.Set(m.x, m.y, col)
 				}
-				// imageLock.Unlock()
 
 				if index.Load() == width*height {
 					passCompleted <- true
@@ -167,12 +161,10 @@ func main() {
 				_index := index.Load()
 				_x := _index % width
 				_y := _index / width
-				if explodesAt[_x][_y] <= 0 {
-					go DoProcess(Pair{
-						x: int(_x),
-						y: int(_y),
-					}, workerCompleted)
-				}
+				go DoProcess(Pair{
+					x: int(_x),
+					y: int(_y),
+				}, workerCompleted)
 				index.Add(1)
 			}
 		}
@@ -182,13 +174,6 @@ func main() {
 		for range 30 {
 			fmt.Println("Iteration", iterations, "started")
 			startTime := time.Now()
-			// q = q[:0]
-			// q = make([]Pair, width*height)
-			// for x := range width {
-			// 	for y := range height {
-			// 		q = append(q, Pair{x: x, y: y})
-			// 	}
-			// }
 
 			index.Store(0)
 
@@ -199,12 +184,10 @@ func main() {
 				_x := _index % width
 				_y := _index / width
 
-				if explodesAt[_x][_y] <= 0 {
-					go DoProcess(Pair{
-						x: int(_x),
-						y: int(_y),
-					}, workerCompleted)
-				}
+				go DoProcess(Pair{
+					x: int(_x),
+					y: int(_y),
+				}, workerCompleted)
 				index.Add(1)
 			}
 
